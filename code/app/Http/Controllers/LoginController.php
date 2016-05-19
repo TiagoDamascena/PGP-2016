f86fb324e7cdfddd790235571ab992f2d669d945
Bruno Messias <brunodsm10@gmail.com>
2016-05-19 16:27:30 -0300

    TASK-04 Finalização do endpoint "Cadastro"

--- code/app/Http/Controllers/LoginController.php
+++ code/app/Http/Controllers/LoginController.php
@@ -8,20 +8,35 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
-use Validator;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class LoginController extends Controller
 {
     public function loginAuthenticate()
     {
+
         $email = Input::get('email');
         $password = Input::get('password');
-        return redirect(url('/errorLogin'));
+
+        $results = DB::select('select password from users where email = $email');
+
+        return $results == $password ? redirect(url('/home')) : redirect(url('/errorLogin'));
+    }
+
+    public function cadastro()
+    {
+        $email = Input::get('email');
+        $password = Input::get('password');
+        $name = Input::get('nome');
+        $confirmPassword = Input::get('confirmPassword');
+
+        $results=false;
+
+        if ($password == $confirmPassword) {
+            $results = DB::insert('insert into users (name, password, email) values ($name,$password,$email)');
+        }
+
+        return $results ? redirect(url('/home')) : redirect(url('/errorLogin'));
     }
 }
