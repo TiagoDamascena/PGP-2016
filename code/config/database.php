115432c0dbe24a3c82880df8f20eaeb07238ddba
Michel Berigo <michelrayesberigo@hotmail.com>
2016-05-18 21:30:29 -0300

    Arquivo database.php contém dados do Banco de Dados

--- code/config/database.php
+++ code/config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'br-cdbr-azure-south-b.cloudapp.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mystudylife'),
+            'username' => env('DB_USERNAME', 'bb129cc90f6ffc'),
+            'password' => env('DB_PASSWORD', 'd038a2a1'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
