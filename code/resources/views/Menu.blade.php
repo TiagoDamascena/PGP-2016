12bc33b30e5982f1ab616ab7440046f430773f7c
Níkolas Matias Xavier Morais <Nkolas Matias Xavier Morais>
2016-05-20 00:32:57 -0300

    A tela menu já foi ajustada para o novo padrão.

--- code/resources/views/Menu.blade.php
+++ code/resources/views/Menu.blade.php
@@ -3,29 +3,21 @@
 <head>
 	<title>home page</title>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" type="text/css" href="../../code/public/Menu.css" />
+	<link rel="stylesheet" type="text/css" href="../../public/Menu.css" />
 </head>
 <body>
 	<header>
-		<h1>Home Page</h1>
+		<h1 class="titulo">Home Page</h1>
+		<ul id="lista">
+			<form id="Menu" method="get" action="{{url('/deleteUser')}}">
+			<li class="Menu" onclick="Menu.submit()">
+				<a href="#">Excluir</a></li>
+			</form>
+			<li>
+			<a class="Menu" href="#">Sair</a></li>
+		</ul>
 	</header>
 	<div>
-		<section>
-			<nav>
-				<ul>
-					<li>
-						<h2>Usuario</h2>
-						<ul>
-							<form id="Menu" method="get" action="{{url('/deleteUser')}}">
-							<li onclick="Menu.submit()">
-								<a href="#">Excluir Conta</a></li>
-							</form>
-							<li><a href="#">Sair da Conta</a></li>
-						</ul>
-					</li>
-				</ul>
-			</nav>
-		</section>
 	</div>
 	<footer>
 		<h2>Trabalho de Gerencia de Projetos</h2>
