55080345c2949f5f9267c04ad5eedcbdb1d937f4
TiagoDamascena <tiagobdamascena@gmail.com>
2016-08-19 15:49:17 -0300

    RELEASE-2.0 Atualizada versão no rodapé da página

--- code/resources/views/layouts/master.blade.php
+++ code/resources/views/layouts/master.blade.php
@@ -82,7 +82,7 @@
 
             <footer class="main-footer">
                 <div class="pull-right hidden-xs">
-                    v0.2.0
+                    v2.0.0
                     <i class="fa fa-star"></i>
                 </div>
                 <strong>Copyright &copy; 2016 <a href="https://github.com/nikolassupremo/PGP-2016">Study Each</a>.</strong> Gerência de Projeto de Software.
