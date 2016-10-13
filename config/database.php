6cee1e20be5d22f72de5ba25568de65e0bf16dbe
TiagoDamascena <tiagobdamascena@gmail.com>
2016-10-13 17:13:51 -0300

    COnfiguração de ambiente introduzida

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'br-cdbr-azure-south-b.cloudapp.net',
-            'port' => '3306',
-            'database' => 'mystudylife',
-            'username' => 'bb129cc90f6ffc',
-            'password' => 'd038a2a1',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
