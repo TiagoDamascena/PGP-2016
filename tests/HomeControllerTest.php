5c94b532baebc92cc1437d2e357966b520aa7e66
TiagoDamascena <tiagobdamascena@gmail.com>
2016-12-08 13:13:32 -0200

    #19 - Testes para HomeController finalizados

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -1,17 +1,38 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
 
 class HomeControllerTest extends TestCase {
 
-    public function testUserNotLogged() {
+    use DatabaseTransactions;
+
+    public function testUserNotLoggedLink() {
+        $this->visit('/home')->seePageIs('/userNotLogged');
+    }
+
+    public function testUserNotLoggedPage() {
         $this->visit('/home')->see('Please Sign in first');
     }
 
-    public function testUserLogged() {
+    public function testUserLoggedLink() {
+        $this->login();
+        $this->visit('/home')->seePageIs('/home');
+    }
 
-        $this->visit('/home')->see('Please Sign in first');
+    public function testUserLoggedPage() {
+        $this->login();
+        $this->visit('/home')->see('Home');
+    }
+
+    private function login() {
+        $user = factory(\App\User::class)->create(
+            ['email' => 'teste@teste.teste',
+                'password' => 'teste',
+                'name' => 'Teste',
+                'creationDate' => '2016-12-8 13:02:00']
+        );
+
+        Auth::login($user, true);
     }
 }
