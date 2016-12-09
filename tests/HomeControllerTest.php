10175f3d4eb45ba61211574a07dcdd780ab4dea3
TiagoDamascena <tiagobdamascena@gmail.com>
2016-12-09 15:05:55 -0200

    #19 - Testes da classe HomeController criados

---
+++ tests/HomeControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
+
+class HomeControllerTest extends TestCase {
+
+    use DatabaseTransactions;
+
+    public function testUserNotLoggedLink() {
+        $this->visit('/home')->seePageIs('/userNotLogged');
+    }
+
+    public function testUserNotLoggedPage() {
+        $this->visit('/home')->see('Please Sign in first');
+    }
+
+    public function testUserLoggedLink() {
+        $this->login();
+        $this->visit('/home')->seePageIs('/home');
+    }
+
+    public function testUserLoggedPage() {
+        $this->login();
+        $this->visit('/home')->see('Home');
+    }
+
+    private function login() {
+        $user = factory(\App\User::class)->create(
+            ['email' => 'teste@teste.teste',
+                'password' => 'teste',
+                'name' => 'Teste',
+                'creationDate' => '2016-12-8 13:02:00']
+        );
+
+        Auth::login($user, true);
+    }
+}
