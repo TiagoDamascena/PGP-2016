ea0e6a071822d7fd0a570d1f9d81e9189b6b9df1
Bruno Messias <brunodsm10@gmail.com>
2016-12-13 03:03:40 -0200

    #17 - Evoluçao do TaskController atraves da modularizaçao e utilizaçao da estrutura de lista

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,11 +7,64 @@
 
 namespace App\Http\Controllers;
 
-class TaskController extends Controller{
-    /**
-     * @return string
-     */
-    public function index(){
+
+use App\SchoolTerm;
+use App\SchoolYear;
+use App\Subject;
+use App\Task;
+use Illuminate\Support\Facades\Response;
+
+class TaskController extends Controller
+{
+    private $list  = [];
+
+    public function index()
+    {
         return view('Tasks');
+    }
+
+    public function getUserTask()
+    {
+        $this->searchTask();
+        $response = Response::json($this->list);
+        return $response;
+    }
+
+    private function searchTask()
+    {
+        $userId = \Auth::User()->id;
+        $this->getSchoolYear($userId);
+    }
+
+    private function getSchoolYear($userId)
+    {
+        $schoolYears = SchoolYear::where('owner', $userId)->get();
+        $this->getSchoolTerm($schoolYears);
+    }
+
+    private function getSchoolTerm($schoolYears)
+    {
+        foreach ($schoolYears as $schoolYear) {
+            $schoolTerms = SchoolTerm::where('year', $schoolYear->id)->get();
+            $this->getSubjects($schoolTerms);
+        }
+    }
+
+    private function getSubjects($schoolTerms)
+    {
+        foreach ($schoolTerms as $schoolTerm) {
+            $subjects = Subject::where('term', $schoolTerm->id)->get();
+            $this->getTaks($subjects);
+        }
+    }
+
+    private function getTaks($subjects)
+    {
+        foreach ($subjects as $subject) {
+            $tasks = Task::where('subject', $subject->id)->get();
+            foreach ($tasks as $task){
+                array_push($this->list,$task);
+            }
+        }
     }
 }
