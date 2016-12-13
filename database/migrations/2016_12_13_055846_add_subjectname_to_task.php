84d7f96f9f2ea6767e5e7743e95b059d52bf67ff
Bruno Messias <brunodsm10@gmail.com>
2016-12-13 04:17:16 -0200

    #17 - Modificaçao do BD para adiçao do nome da disciplina

---
+++ database/migrations/2016_12_13_055846_add_subjectname_to_task.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSubjectnameToTask extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('tasks', function ($table){
+           $table->string('subject_name');
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        {
+            Schema::table('tasks', function ($table){
+                $table->dropColumn('subject_name');
+            });
+        }
+    }
+}
