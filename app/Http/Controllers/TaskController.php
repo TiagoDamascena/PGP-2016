4e434a9ff3676c7e10f3796c12dfdb75c465e10d
Bruno Messias <brunodsm10@gmail.com>
2016-12-13 04:39:24 -0200

    #17 - Correçao de codigo para adesao ao padrao

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,7 +7,6 @@
 
 namespace App\Http\Controllers;
 
-
 use App\SchoolTerm;
 use App\SchoolYear;
 use App\Subject;
@@ -18,51 +17,44 @@ class TaskController extends Controller
 {
     private $list  = [];
 
-    public function index()
-    {
+    public function index() {
         return view('Tasks');
     }
 
-    public function getUserTask()
-    {
+    public function getUserTask() {
         $this->searchTask();
         $response = Response::json($this->list);
         return $response;
     }
 
-    private function searchTask()
-    {
+    private function searchTask() {
         $userId = \Auth::User()->id;
         $this->getSchoolYear($userId);
     }
 
-    private function getSchoolYear($userId)
-    {
+    private function getSchoolYear($userId) {
         $schoolYears = SchoolYear::where('owner', $userId)->get();
         $this->getSchoolTerm($schoolYears);
     }
 
-    private function getSchoolTerm($schoolYears)
-    {
+    private function getSchoolTerm($schoolYears) {
         foreach ($schoolYears as $schoolYear) {
             $schoolTerms = SchoolTerm::where('year', $schoolYear->id)->get();
             $this->getSubjects($schoolTerms);
         }
     }
 
-    private function getSubjects($schoolTerms)
-    {
+    private function getSubjects($schoolTerms) {
         foreach ($schoolTerms as $schoolTerm) {
             $subjects = Subject::where('term', $schoolTerm->id)->get();
             $this->getTaks($subjects);
         }
     }
 
-    private function getTaks($subjects)
-    {
+    private function getTaks($subjects) {
         foreach ($subjects as $subject) {
             $tasks = Task::where('subject', $subject->id)->get();
-            foreach ($tasks as $task){
+            foreach ($tasks as $task) {
                 array_push($this->list,$task);
             }
         }
